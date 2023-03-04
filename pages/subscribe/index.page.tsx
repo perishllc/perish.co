d40bd4daddf75311025b3f6bf64dcb75681361f2
fossephate <matt@fosse.co>
2023-03-04 20:25:23 +0000

    save

--- pages/subscribe/index.page.tsx
+++ pages/subscribe/index.page.tsx
@@ -1,31 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
 import Nav from "/src/modules/Nav";
 import Footer from "/src/modules/Footer";
 
 import { QRCodeSVG } from "qrcode.react";
 import { generate } from "nano-uri";
+import { tools } from "nanocurrency-web";
 
 import { decode } from "js-base64";
 
 export { Page }
 
 function Page() {
-
-  // get current day of month:
-
-
-  let request = {
+  const [request, setRequest] = useState({
     account: "nano_38713x95zyjsqzx6nm1dsom1jmm668owkeb9913ax6nfgj15az3nu8xkx579",
-    amount: "100000000000000000000000000000",
+    amount: "1000000000000000000000000000000",
     label: "",
-    message: "Nautilus Donation",
-    frequency: `0 0 ${new Date().getDate()} * *`
+    message: "",
+    frequency: `0 0 ${new Date().getDate()} * *`,
+  });
+
+  const handleAmountChange = (event: any) => {
+    let value = event.target.value;
+    if (value == "") {
+      value = "0";
+    }
+
+    const converted = tools.convert(value, 'NANO', 'RAW');
+
+    setRequest({ ...request, amount: converted });
+  };
+
+  const handleLabelChange = (event: any) => {
+    setRequest({ ...request, label: event.target.value });
+  };
+
+  const handleMessageChange = (event: any) => {
+
+    setRequest({ ...request, message: event.target.value });
+  };
+
+  const handleAccountChange = (event: any) => {
+    let value = event.target.value;
+    if (value == "") {
+      value = "nano_38713x95zyjsqzx6nm1dsom1jmm668owkeb9913ax6nfgj15az3nu8xkx579";
+    }
+
+    setRequest({ ...request, account: value });
+  };
+
+  const handleFrequencyChange = (event: any) => {
+    if (event.target.value == "") {
+      event.target.value = `0 0 ${new Date().getDate()} * *`;
+    }
+
+    setRequest({ ...request, frequency: event.target.value });
   };
 
   let nanoStr = generate.sub(request);
-  console.log(nanoStr);
-  console.log(JSON.parse(decode(nanoStr.substring(8))));
-
 
   return (
     <div className="page bg-white dark:bg-gray-800 text-black dark:text-white w-full min-h-screen">
@@ -38,9 +69,51 @@ function Page() {
             </div>
           </a>
         </div>
+        <div className="flex flex-col justify-center mt-6 text-black">
+          <div className="flex flex-row justify-center mb-3">
+            <input
+              type="text"
+              placeholder="Account"
+              // value={request.account}
+              onChange={handleAccountChange}
+            />
+          </div>
+          <div className="flex flex-row justify-center mb-3">
+            <input
+              type="text"
+              placeholder="Amount"
+              // value={request.amount}
+              onChange={handleAmountChange}
+            />
+          </div>
+          <div className="flex flex-row justify-center mb-3">
+            <input
+              type="text"
+              placeholder="Label"
+              value={request.label}
+              onChange={handleLabelChange}
+            />
+          </div>
+          <div className="flex flex-row justify-center mb-3">
+            <input
+              type="text"
+              placeholder="Message"
+              value={request.message}
+              onChange={handleMessageChange}
+            />
+          </div>
+          <div className="flex flex-row justify-center mb-3">
+            <input
+              type="text"
+              placeholder="Frequency"
+              // value={request.frequency}
+              onChange={handleFrequencyChange}
+            />
+          </div>
+        </div>
+
       </div>
       <Footer />
     </div>
   );
-
 }
